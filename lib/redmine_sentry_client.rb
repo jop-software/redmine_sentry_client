97066dbe01777eab4958d9985e9ae167aedefceb
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-09-03 21:49:21 +0200

    [TASK] Move settings functions into its own module / class

--- lib/redmine_sentry_client.rb
+++ lib/redmine_sentry_client.rb
@@ -1,3 +1,23 @@
 require 'redmine_sentry_client/helper/sentry_helper'
 
+module RedmineSentryClient
+
+    class << self
+
+        def traces_sample_rate
+            Setting.plugin_redmine_sentry_client['traces_sample_rate'].to_d || 0.5
+        end
+
+        def environment
+            return Setting.plugin_redmine_sentry_client['environment'] ? Setting.plugin_redmine_sentry_client['environment'] : Rails.env
+        end
+    
+        def dsn
+            return Setting.plugin_redmine_sentry_client['dsn'] || ""
+        end
+
+    end
+
+end
+
 RedmineSentryClient::Helper::SentryHelper.init()
