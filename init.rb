07448c847d576bfcdd23b34ca2435d9f87c0cd0e
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-09-03 19:34:48 +0200

    [TASK] Update repository link
    Related: #5

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_sentry_client do
   author 'jop-software Inh. Johannes Przymusinski'
   description 'This plugins integrates the sentry sdk to report internal redmine errors to sentry'
   version '0.0.1'
-  url 'https://github.com/jop-software/redmine-sentry-client'
+  url 'https://github.com/jop-software/redmine_sentry_client'
   author_url 'http://jop-software.de'
   settings default: {'empty' => true}, partial: 'settings/sentry_settings'
 end
