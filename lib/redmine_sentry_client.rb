dd5ad7520309c8bf5b69b017460f6b55892d78da
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2022-08-24 21:17:19 +0200

    feat: add SENTRY_ACTIVE environment variable

--- lib/redmine_sentry_client.rb
+++ lib/redmine_sentry_client.rb
@@ -5,6 +5,16 @@ require ::File.expand_path('redmine_sentry_client/helper/sentry_helper', __dir__
 # Helper library to integrate sentry
 module RedmineSentryClient
   class << self
+    # return SENTRY_ACTIVE environment variable.
+    # If the environment variable is not set, fallback to true.
+    def active
+      if ENV['SENTRY_ACTIVE'].present?
+        ENV['SENTRY_ACTIVE']
+      else
+        true
+      end
+    end
+
     def traces_sample_rate
       Setting.plugin_redmine_sentry_client['traces_sample_rate'].to_d || 0.5
     end
