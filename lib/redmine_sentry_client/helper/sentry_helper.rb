e32ad3d54ba0836436afd3846fb773b6e191c285
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2022-08-24 21:57:17 +0200

    task: use single instead of double quotes

--- lib/redmine_sentry_client/helper/sentry_helper.rb
+++ lib/redmine_sentry_client/helper/sentry_helper.rb
@@ -6,7 +6,7 @@ module RedmineSentryClient
     class SentryHelper
       def self.init
         # Check if the sentry integration is active.
-        return if RedmineSentryClient.active != "true"
+        return if RedmineSentryClient.active != 'true'
 
         Sentry.init do |config|
           config.dsn = RedmineSentryClient.dsn
